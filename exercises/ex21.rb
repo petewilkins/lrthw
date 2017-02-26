fc4ae723b230d23a669d6db508dd2fb45506b60e
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-02-26 01:53:58 +0000

    Exercise 21

---
+++ exercises/ex21.rb
@@ -0,0 +1,35 @@
+def add(a, b)
+    puts "ADDING #{a} + #{b}"
+    return a + b
+end
+
+def subtract(a, b)
+    puts "SUBTRACTING #{a} - #{b}"
+    return a - b
+end
+
+def multiply(a, b)
+    puts "MULTIPLYING #{a} * #{b}"
+    return a * b 
+end
+
+def divide(a, b)
+    puts "DIVIDING #{a} / #{b}"
+    return a / b 
+end
+
+puts "Let's do some math with just functions!"
+
+age = add(20, 7)
+height = subtract(298, 105)
+weight = multiply(44, 2)
+iq = divide(400, 2)
+
+puts "Age: #{age}, Height: #{height}, Weight: #{weight}, IQ: #{iq}."
+
+# A puzzle for the extra credit, type it in anyway.
+puts "Here is a puzzle."
+
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+puts "That becomes: #{what}. Can you do it by hand?"
